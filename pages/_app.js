c2372c64f009234db69c40c4c3bc16be812a42f5
ma9pie <ma9pie.dev@gmail.com>
2022-11-07 21:32:05 +0900

    애널리틱스 추가 및 reset.scss 추가

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,30 @@
-import { Global } from "@emotion/react";
 import styled from "@emotion/styled";
-import React, { useState } from "react";
+import { useRouter } from "next/router";
+import React, { useEffect, useState } from "react";
 import { Hydrate, QueryClient, QueryClientProvider } from "react-query";
 import { ReactQueryDevtools } from "react-query/devtools";
+import GoogleAnalyticsUtils from "@/utils/GoogleAnalyticsUtils";
 import "@/styles/fonts.scss";
 import "@/styles/globals.scss";
-import reset from "@/styles/reset";
+import "@/styles/reset.scss";
+import "@/styles/simplebar.scss";
 
 function App({ Component, pageProps }) {
+  const router = useRouter();
   const getLayout = Component.getLayout || ((page) => page);
 
   const [queryClient] = useState(() => new QueryClient());
+
+  // 구글 애널리틱스 조회수 측정
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      GoogleAnalyticsUtils.changeRouteGtag(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
 
   return (
     <QueryClientProvider client={queryClient}>
@@ -20,7 +34,6 @@ function App({ Component, pageProps }) {
             <ReactQueryDevtools initialIsOpen={false} />
           )}
       </ReactQueryDevtoolsWrapper>
-      <Global styles={reset}></Global>
       <Hydrate state={pageProps?.dehydrateState}>
         {getLayout(<Component {...pageProps} />)}
       </Hydrate>
--- pages/_document.js
+++ pages/_document.js
@@ -4,6 +4,29 @@ export default function Document() {
   return (
     <Html>
       <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
+        />
+        {/* 구글 애널리틱스 */}
+        <script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_TRACKING_ID}`}
+        />
+        <script
+          id="google-analytics"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', '${process.env.NEXT_PUBLIC_GA_TRACKING_ID}', {
+                page_path: window.location.pathname,
+              });
+            `,
+          }}
+        />
         {/* 다크모드 시 화면 깜빡임 제거 */}
         <script
           dangerouslySetInnerHTML={{
