7201ed7f29ef412b4943b1a2e95f08b1271c9629
ma9pie <ma9pie.dev@gmail.com>
2022-12-11 03:02:06 +0900

    recoil persist 추가

--- recoil/atom.js
+++ recoil/atom.js
@@ -1,8 +1,13 @@
 import { atom } from "recoil";
+import { recoilPersist } from "recoil-persist";
+
+const { persistAtom } = recoilPersist();
+const timestamp = new Date().getTime();
 
 export const countState = atom({
   key: "countState",
   default: 0,
+  effects_UNSTABLE: [persistAtom],
 });
 
 export const todoListState = atom({
@@ -14,14 +19,17 @@ export const todoListState = atom({
     { contents: "list 4", importance: "HIGH", status: "HOLD" },
     { contents: "list 5", importance: "LOW", status: "DOING" },
   ],
+  effects_UNSTABLE: [persistAtom],
 });
 
 export const filterState = atom({
   key: "filterState",
   default: { importance: "", status: "" },
+  effects_UNSTABLE: [persistAtom],
 });
 
 export const themeState = atom({
   key: "themeState",
   default: "Dark",
+  effects_UNSTABLE: [persistAtom],
 });
