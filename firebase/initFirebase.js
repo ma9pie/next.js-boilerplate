0cf9f52ace37b29b44760b447394e0d42e584593
ma9pie <ma9pie.dev@gmail.com>
2022-12-25 17:06:18 +0900

    firebase 환경변수 설정

--- firebase/initFirebase.js
+++ firebase/initFirebase.js
@@ -2,13 +2,13 @@ import firebase from "firebase/compat/app";
 import "firebase/compat/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDSSRAhLBMo6XuGTfiOwTM5JMk9GVpCSmE",
-  authDomain: "boilerplate-b8f9c.firebaseapp.com",
-  projectId: "boilerplate-b8f9c",
-  storageBucket: "boilerplate-b8f9c.appspot.com",
-  messagingSenderId: "507303788259",
-  appId: "1:507303788259:web:3f86350d42efd5195dcfd6",
-  measurementId: "G-EVSTQRGNF1",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 firebase.initializeApp(firebaseConfig);
@@ -16,12 +16,3 @@ firebase.initializeApp(firebaseConfig);
 const firestore = firebase.firestore();
 
 export { firestore };
-
-// const firebaseConfig = {
-//     apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-//     authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-//     projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-//     storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-//     messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-//     appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-// };
