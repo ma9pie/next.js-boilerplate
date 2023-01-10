475e766fef30ff36843231c5ca1c1adc1a9ca210
김준영 <kjy1787@newlinkcorp.co.kr>
2023-01-10 17:33:50 +0900

    useThrottle, useDebounce 고도화

--- hooks/useDebounce.js
+++ hooks/useDebounce.js
@@ -1,18 +1,49 @@
-import { useCallback } from "react";
+/**
+ * @refference
+ * https://learnersbucket.com/examples/interview/usedebounce-hook-in-react/
+ */
+import { useCallback, useRef } from "react";
 
-let timeout;
-const useDebounce = (func, wait) => {
-  return useCallback(
-    (...args) => {
-      const context = this;
-      if (timeout) clearTimeout(timeout);
-      timeout = setTimeout(() => {
-        timeout = null;
-        func.apply(context, args);
-      }, wait || 300);
+const useDebounce = (callback, delay = 300, immediate = false) => {
+  // ref the timer
+  const timerId = useRef();
+
+  // create a memoized debounce
+  const debounce = useCallback(
+    function () {
+      // reference the context and args for the setTimeout function
+      let context = this,
+        args = arguments;
+
+      // should the function be called now? If immediate is true
+      // and not already in a timeout then the answer is: Yes
+      const callNow = immediate && !timerId.current;
+
+      // base case
+      // clear the timeout to assign the new timeout to it.
+      // when event is fired repeatedly then this helps to reset
+      clearTimeout(timerId.current);
+
+      // set the new timeout
+      timerId.current = setTimeout(function () {
+        // Inside the timeout function, clear the timeout variable
+        // which will let the next execution run when in 'immediate' mode
+        timerId.current = null;
+
+        // check if the function already ran with the immediate flag
+        if (!immediate) {
+          // call the original function with apply
+          callback.apply(context, args);
+        }
+      }, delay);
+
+      // immediate mode and no wait timer? Execute the function immediately
+      if (callNow) callback.apply(context, args);
     },
-    [func, wait]
+    [callback, delay, immediate]
   );
+
+  return debounce;
 };
 
 export default useDebounce;
--- hooks/useThrottle.js
+++ hooks/useThrottle.js
@@ -1,19 +1,51 @@
-import { useCallback } from "react";
+/**
+ * @refference
+ * https://learnersbucket.com/examples/interview/usethrottle-hook-in-react/
+ */
+import { useCallback, useRef } from "react";
 
-let timeout;
-const useThrottle = (func, wait) => {
-  return useCallback(
-    (...args) => {
-      const context = this;
-      if (!timeout) {
-        timeout = setTimeout(() => {
-          func.apply(context, args);
-          timeout = null;
-        }, wait || 300);
+const useThrottle = (
+  callback,
+  wait = 300,
+  option = { leading: true, trailing: true }
+) => {
+  const timerId = useRef(); // track the timer
+  const lastArgs = useRef(); // track the args
+
+  // create a memoized debounce
+  const throttle = useCallback(
+    function (...args) {
+      const { trailing, leading } = option;
+      // function for delayed call
+      const waitFunc = () => {
+        // if trailing invoke the function and start the timer again
+        if (trailing && lastArgs.current) {
+          callback.apply(this, lastArgs.current);
+          lastArgs.current = null;
+          timerId.current = setTimeout(waitFunc, wait);
+        } else {
+          // else reset the timer
+          timerId.current = null;
+        }
+      };
+
+      // if leading run it right away
+      if (!timerId.current && leading) {
+        callback.apply(this, args);
+      }
+      // else store the args
+      else {
+        lastArgs.current = args;
+      }
+
+      // run the delayed call
+      if (!timerId.current) {
+        timerId.current = setTimeout(waitFunc, wait);
       }
     },
-    [func, wait]
+    [callback, wait, option]
   );
-};
 
+  return throttle;
+};
 export default useThrottle;
